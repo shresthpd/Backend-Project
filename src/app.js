a3d828b3d10e5c7761452726ebe8375e4cda2696
Shresth Prasad <prasadshresth04@gmail.com>
2024-06-27 15:09:21 +0530

    all setup done

--- src/app.js
+++ src/app.js
@@ -24,4 +24,7 @@ app.use(
 app.use(express.static("public"));
 app.use(cookieParser());
 
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
@@ -16,8 +16,7 @@ connectDB()
   });
 
 /*
-import express from "express";
-const app = express();
+
 
 (async () => {
   try {
